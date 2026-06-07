d209765df737599d83b1057c573ea3d8fd5cd5d3
Nikithasri <ynikithasri@gmail.com>
2026-06-07 16:53:17 +0530

    Added Day-11 notebook

--- sql assignment - Copy/main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("student_data.csv")
-
-print(df)
--- sql assignment - Copy/queries.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO student_data(student_id,first_name,last_name,Email,Gender)
-VALUES(1,'Alice','Smith','alice.smith@gmail.com','Female'),
-(2,'Bob','Johnson','bob.johnson@gmail.com','Male'),
-(3,'charlie','Brown','charlie.brown@gmail.com','Male'),
-(4,'Diana','Prince','diana.Prince@gmail.com','Female');
-select*
-from student_data
-select student_id,first_name
-from student_data
